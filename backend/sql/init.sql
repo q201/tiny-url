ab398786790656da3541f304c6d370d90aae6e39
q-zaydu <quasim@zaydu.com>
2025-11-20 02:49:50 +0530

    correcting sql file

--- backend/sql/init.sql
+++ backend/sql/init.sql
@@ -1,10 +1,8 @@
 -- SQL init script for TinyLink
 
 CREATE TABLE IF NOT EXISTS links (
-  id SERIAL PRIMARY KEY,
-  code VARCHAR(8) NOT NULL UNIQUE,
-  long_url TEXT NOT NULL,
+  short_code VARCHAR(8) NOT NULL UNIQUE,
+  target_url TEXT NOT NULL,
   total_clicks INTEGER NOT NULL DEFAULT 0,
   last_clicked_time TIMESTAMP NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT now()
 );
